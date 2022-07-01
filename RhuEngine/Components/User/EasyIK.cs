565d20b36697bfcb1112c1ea17390cbd028aa091
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-07-01 11:10:32 -0500

    Refactor

--- RhuEngine/Components/User/EasyIK.cs
+++ RhuEngine/Components/User/EasyIK.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
+﻿using System.Linq;
 
 using RhuEngine.WorldObjects.ECS;
 
@@ -9,7 +6,7 @@ using RNumerics;
 
 namespace RhuEngine.Components
 {
-	public class EasyIK
+	public class EasyIK : Component
 	{
 		public int numberOfJoints = 2;
 		public Matrix ikTarget;
@@ -36,7 +33,11 @@ namespace RhuEngine.Components
 		public bool poleDirection = false;
 		public bool poleRotationAxis = false;
 
-		void Awake() {
+		public override void OnLoaded() {
+			Reload();
+		}
+
+		private void Reload() {
 			// Let's set some properties
 			jointChainLength = 0;
 			jointTransforms = new Matrix[numberOfJoints];
@@ -47,11 +48,11 @@ namespace RhuEngine.Components
 			ikTargetStartRot = ikTarget.Rotation;
 
 			// TODO Get current gameobject
-			var current = this;
+			var current = Entity;
 
 			// For each bone calculate and store the lenght of the bone
 			for (var i = 0; i < jointTransforms.Length; i += 1) {
-				jointTransforms[i] = current;
+				jointTransforms[i] = current.GlobalTrans;
 				// If the bones lenght equals the max lenght, we are on the last joint in the chain
 				if (i == jointTransforms.Length - 1) {
 					lastJointStartRot = current.rotation;
@@ -59,46 +60,46 @@ namespace RhuEngine.Components
 				}
 				// Store length and add the sum of the bone lengths
 				else {
-					boneLength[i] = Vector3.Distance(current.position, current.GetChild(0).position);
+					boneLength[i] = current.position.Value.Distance(current.children[0].position);
 					jointChainLength += boneLength[i];
 
-					jointStartDirection[i] = current.GetChild(0).position - current.position;
+					jointStartDirection[i] = current.children[0].position.Value - current.position;
 					startRotation[i] = current.rotation;
 				}
 				// Move the iteration to next joint in the chain
-				current = current.GetChild(0);
+				current = current.children[0];
 			}
 		}
 
 		void PoleConstraint() {
 			if (poleTarget != null && numberOfJoints < 4) {
 				// Get the limb axis direction
-				var limbAxis = Vector3.Normalize(jointPositions[2] - jointPositions[0]);
+				var limbAxis = (jointPositions[2] - jointPositions[0]).Normalized;
 
 				// Get the direction from the root joint to the pole target and mid joint
-				Vector3 poleDirection = Vector3.Normalize(poleTarget.Translation - jointPositions[0]);
-				Vector3 boneDirection = Vector3.Normalize(jointPositions[1] - jointPositions[0]);
+				var poleDirection = (poleTarget.Translation - jointPositions[0]).Normalized;
+				var boneDirection = (jointPositions[1] - jointPositions[0]).Normalized;
 
 				// Ortho-normalize the vectors
 				// TODO Implement a Gram-Schmidt Orthonormaliztion function
-				// Vector3.OrthoNormalize(ref limbAxis, ref poleDirection);
-				// Vector3.OrthoNormalize(ref limbAxis, ref boneDirection);
-				var temp = Vector3.Cross(limbAxis, poleDirection);
-				poleDirection = Vector3.Cross(temp, limbAxis);
-				temp = Vector3.Cross(limbAxis, boneDirection);
-				boneDirection = Vector3.Cross(temp, limbAxis);
+				// Vector3f.OrthoNormalize(ref limbAxis, ref poleDirection);
+				// Vector3f.OrthoNormalize(ref limbAxis, ref boneDirection);
+				var temp = Vector3f.Cross(limbAxis, poleDirection);
+				poleDirection = Vector3f.Cross(temp, limbAxis);
+				temp = Vector3f.Cross(limbAxis, boneDirection);
+				boneDirection = Vector3f.Cross(temp, limbAxis);
 
 				// Calculate the angle between the boneDirection vector and poleDirection vector
-				Quaternionf angle = Quaternionf.FromToRotation(boneDirection, poleDirection);
+				var angle = Quaternionf.FromToRotation(boneDirection, poleDirection);
 
 				// Rotate the middle bone using the angle
-				jointPositions[1] = angle * (jointPositions[1] - jointPositions[0]) + jointPositions[0];
+				jointPositions[1] = (angle * (jointPositions[1] - jointPositions[0])) + jointPositions[0];
 			}
 		}
 
 		void Backward() {
 			// Iterate through every position in the list until we reach the start of the chain
-			for (int i = jointPositions.Length - 1; i >= 0; i -= 1) {
+			for (var i = jointPositions.Length - 1; i >= 0; i -= 1) {
 				// The last bone position should have the same position as the ikTarget
 				if (i == jointPositions.Length - 1) {
 					jointPositions[i] = ikTarget.Translation;
@@ -111,7 +112,7 @@ namespace RhuEngine.Components
 
 		void Forward() {
 			// Iterate through every position in the list until we reach the end of the chain
-			for (int i = 0; i < jointPositions.Length; i += 1) {
+			for (var i = 0; i < jointPositions.Length; i += 1) {
 				// The first bone position should have the same position as the startPosition
 				if (i == 0) {
 					jointPositions[i] = startPosition;
@@ -124,11 +125,11 @@ namespace RhuEngine.Components
 
 		private void SolveIK() {
 			// Get the jointPositions from the joints
-			for (int i = 0; i < jointTransforms.Length; i += 1) {
+			for (var i = 0; i < jointTransforms.Length; i += 1) {
 				jointPositions[i] = jointTransforms[i].Translation;
 			}
 			// Distance from the root to the ikTarget
-			distanceToTarget = Vector3.Distance(jointPositions[0], ikTarget.Translation);
+			distanceToTarget = jointPositions[0].DistanceSquared(ikTarget.Translation);
 
 			// IF THE TARGET IS NOT REACHABLE
 			if (distanceToTarget > jointChainLength) {
@@ -136,15 +137,15 @@ namespace RhuEngine.Components
 				var direction = ikTarget.Translation - jointPositions[0];
 
 				// Get the jointPositions
-				for (int i = 1; i < jointPositions.Length; i += 1) {
+				for (var i = 1; i < jointPositions.Length; i += 1) {
 					jointPositions[i] = jointPositions[i - 1] + direction.Normalized * boneLength[i - 1];
 				}
 			}
 			// IF THE TARGET IS REACHABLE
 			else {
 				// Get the distance from the leaf bone to the ikTarget
-				float distToTarget = Vector3.Distance(jointPositions[jointPositions.Length - 1], ikTarget.Translation);
-				float counter = 0;
+				var distToTarget = jointPositions[jointPositions.Length - 1].DistanceSquared(ikTarget.Translation);
+				var counter = 0;
 				// While the distance to target is greater than the tolerance let's iterate until we are close enough
 				while (distToTarget > tolerance) {
 					startPosition = jointPositions[0];
@@ -161,14 +162,14 @@ namespace RhuEngine.Components
 			PoleConstraint();
 
 			// Apply the jointPositions and rotations to the joints
-			for (int i = 0; i < jointPositions.Length - 1; i += 1) {
+			for (var i = 0; i < jointPositions.Length - 1; i += 1) {
 				jointTransforms[i].Translation = jointPositions[i];
-				var targetRotation = Quaternionf.FromToRotation(jointStartDirection[i], jointPositions[i + 1] - jointPositions[i]); // We're mixing and matching here
+				var targetRotation = Quaternionf.FromToRotation(jointStartDirection[i], jointPositions[i + 1] - jointPositions[i]);
 				var temp = targetRotation * startRotation[i];
-				jointTransforms[i].Rotation[0] = temp[0];
+				jointTransforms[i].Rotation = temp[0];
 			}
 			// Let's constrain the rotation of the last joint to the IK target and maintain the offset.
-			Quaternion offset = lastJointStartRot * Quaternion.Inverse(ikTargetStartRot);
+			var offset = lastJointStartRot * ikTargetStartRot.Inverse;
 			jointTransforms.Last().Rotation = ikTarget.Rotation * offset;
 		}
 
